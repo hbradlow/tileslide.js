140d8f26f0cb09af2812d3ca4bad1ae47cd33040
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-13 02:02:44 -0700

    Fixed z-index problem

--- tileslide.js
+++ tileslide.js
@@ -5,10 +5,15 @@
         var img_height = 200;
         var text_height = 100;
         $(this).hover(function(){
+            $(this).css("z-index","100");
             $(this).find("img").stop().animate({"top":"-"+slide_height+"px"},slide_time);
             $(this).find(".tile_text").stop().animate({"top":String(img_height-slide_height)+"px"},slide_time);
         },function(){
-            $(this).find("img").stop().animate({"top":"0px"},slide_time);
+            var tile = this;
+            $(tile).css("z-index","99");
+            $(this).find("img").stop().animate({"top":"0px"},slide_time,function(){
+                $(tile).css("z-index","1");
+            });
             $(this).find(".tile_text").stop().animate({"top":String(img_height-text_height)+"px"},slide_time);
         });
     };
