ae503632f56def6de3114081f69fb46442fcfe75
Henry Bradlow <hbradlow@berkeley.edu>
2012-10-13 18:03:45 -0700

    Added options to function

--- tileslide.js
+++ tileslide.js
@@ -1,20 +1,28 @@
 (function($){
-    $.fn.tileslide = function(){
-        var slide_time = 300;
-        var slide_height = 50;
-        var img_height = 200;
-        var text_height = 100;
+    $.fn.tileslide = function(options){
+        if(!options) options = {};
+        var default_args = {
+            "slide_time":   300,
+            "slide_height": 50,
+        }
+        for(var arg in default_args){
+            if(typeof options[arg] == "undefined"){
+                options[arg] = default_args[arg];
+            }
+        }
+        var img_height = parseInt($(this).find(".tile_img").css("height"));
+        var text_height = parseInt($(this).find(".tile_text").css("height"));
         $(this).hover(function(){
             $(this).css("z-index","100");
-            $(this).find("img").stop().animate({"top":"-"+slide_height+"px"},slide_time);
-            $(this).find(".tile_text").stop().animate({"top":String(img_height-slide_height)+"px"},slide_time);
+            $(this).find(".tile_img").stop().animate({"top":"-"+options["slide_height"]+"px"},options["slide_time"]);
+            $(this).find(".tile_text").stop().animate({"top":String(img_height-options["slide_height"])+"px"},options["slide_time"]);
         },function(){
             var tile = this;
             $(tile).css("z-index","99");
-            $(this).find("img").stop().animate({"top":"0px"},slide_time,function(){
+            $(this).find(".tile_img").stop().animate({"top":"0px"},options["slide_time"],function(){
                 $(tile).css("z-index","1");
             });
-            $(this).find(".tile_text").stop().animate({"top":String(img_height-text_height)+"px"},slide_time);
+            $(this).find(".tile_text").stop().animate({"top":String(img_height-text_height)+"px"},options["slide_time"]);
         });
     };
 })(jQuery);
